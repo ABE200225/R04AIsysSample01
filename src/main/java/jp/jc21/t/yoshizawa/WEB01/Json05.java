4dbbf29301597eae3a06cd142e01f87ebc249394
涼 阿部 <200225@jc-21.jp>
2022-10-27 15:00:12 +0900

    Sentiment.javaが動くようになった

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
@@ -21,9 +21,9 @@ public class Json05 {
 	static Language getLanguage(String s) throws IOException, URISyntaxException, InterruptedException {
 		Gson gson = new Gson();
 
-		String url = "https://r04jk3ab-text.cognitiveservices.azure.com//" + "text/analytics/v3.0/languages";
+		String url = "https://r04jk3a01-text.cognitiveservices.azure.com/" + "text/analytics/v3.0/Languages";
 		Map<String, String> map = new HashMap<>();
-		map.put("Ocp-Apim-Subscription-Key", "953ea066e1384085b69e5bd5cb3c20d0");
+		map.put("Ocp-Apim-Subscription-Key", "30be10474efc4d4fae070cf4643ff113");
 
 		Docs doc = new Docs();
 		doc.id = "1";
@@ -60,6 +60,8 @@ class Documents {
 
 class DetectedLanguage {
 	String name;
+	String iso6391Name;
+	
 }
 
 class Source {
